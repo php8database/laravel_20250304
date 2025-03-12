86036f9342c07aac5f3aefb8f6c37a86f0450b53
php8database <php8database@gmail.com>
2025-03-12 11:35:15 +0800

    建立資料庫 1 to 1 index、add other column、fix 與建立新增、修改、刪除功能 (刪除主表與子表_create del)

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Student;
+use App\Models\Phone;
 
 class StudentController extends Controller
 {
@@ -13,10 +14,14 @@ class StudentController extends Controller
      */
     public function index()
     {
+        // dd('student index ok');
         // $users = DB::table('users')->get();
         // $data = DB::table('students')->get();
-        $data = Student::get();
-        // dd($data);
+        // $data = Student::get();
+
+        // $phone = User::find(1)->phone;
+        $data = Student::with('phone')->get();
+        // dd($data[0]->phone);
         // dd($data);
 
         return view('student.index', ['data' => $data]);
@@ -40,18 +45,20 @@ class StudentController extends Controller
         $input = $request->except('_token');
         // dd($input);
 
+        // 主表
         $data = new Student;
-
-        // $data->name = $request->name;
-        // $data->mobile = $request->mobile;
-
         $data->name = $input['name'];
         $data->mobile = $input['mobile'];
-
         $data->save();
 
+        // 子表
+        $item = new Phone;
+        $item->student_id = $data->id;
+        $item->phone = $input['phone'];
+        $item->save();
+
+
         return redirect()->route('students.index');
-        // return redirect('/students');
     }
 
     /**
@@ -101,11 +108,13 @@ class StudentController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(string $id) 
     {
-        // dd("hello destroy $id");
-        $data = Student::where('id', $id)->first();
-        $data->delete();
+        // 刪除子表
+        Phone::where('student_id', $id)->delete();
+        // 刪除主表
+        Student::where('id', $id)->delete();
+
         return redirect()->route('students.index');
     }
 
